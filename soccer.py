07423d0d7d220ddf726893f9f641c2f4977c7485
wodnjj park <cgk7522@naver.com>
2023-06-13 01:18:06 +0000

    수정

--- soccer.py
+++ soccer.py
@@ -6,7 +6,7 @@ premi_team_rank = requests.get(naver_wfootball)
 premi_team_rank_list = BeautifulSoup(premi_team_rank.content, "html.parser", from_encoding='utf-8')
 
 team_rank_list = premi_team_rank_list.select('#_team_rank_epl > table > tbody > tr')
-
+# 유럽대항전 나갈 수 있는 제한 된 순위
 for i, o in enumerate(team_rank_list):
     if i >= 7:
         break
